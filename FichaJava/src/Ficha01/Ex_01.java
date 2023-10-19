5cfd39be9cc52d0a1a1691d215f47e067566d9db
RichieCesae <ricardo.ans29@gmail.com>
2023-10-19 11:53:38 +0100

    Ficha01
    Ex01

--- FichaJava/src/Ficha01/Ex_01.java
+++ FichaJava/src/Ficha01/Ex_01.java
@@ -1,4 +1,31 @@
 package Ficha01;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+
+    public static void main(String[] args) {
+
+        //instanciar scanner
+        Scanner input = new Scanner(System.in);
+
+        //declarar variaveis
+        int num1, num2, soma;
+
+        //ler num1
+        System.out.print("Introduza um número: ");
+        num1 = input.nextInt();
+
+        //ler num2
+        System.out.print("Introduza outro número: ");
+        num2 = input.nextInt();
+
+        //calcular a soma de num1 + num2
+        soma = num1+num2;
+
+        // apresentar a soma
+        System.out.println("Soma: " + soma);
+
+    }
+
 }

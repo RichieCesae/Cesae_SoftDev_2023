171be747d6dc8b2e398eb6b586e17e5ff90aaf21
RichieCesae <ricardo.ans29@gmail.com>
2023-10-19 12:24:19 +0100

    ficha01
    ex2,3,4,5

--- FichaJava/src/Ficha01/Ex_05.java
+++ FichaJava/src/Ficha01/Ex_05.java
@@ -1,4 +1,40 @@
 package Ficha01;
 
+import java.util.Scanner;
+
 public class Ex_05 {
+
+    public static void main(String[] args) {
+
+        //instanciar scanner
+        Scanner input = new Scanner(System.in);
+
+        //declarar variaveis
+        double num1, num2, num3, soma, media;
+
+        //ler num1
+        System.out.print("Introduza um número: ");
+        num1 = input.nextInt();
+
+        //ler num2
+        System.out.print("Introduza um número: ");
+        num2 = input.nextInt();
+
+        //ler num3
+        System.out.print("Introduza um número: ");
+        num3 = input.nextInt();
+
+        //calcular a media
+        soma = num1+num2+num3;
+        media = soma/3;
+
+        // apresentar a media
+        System.out.println("Media dos numeros escolhidos: " + media);
+
+        //apresentar media ponderada 20%
+        
+
+
+    }
+
 }

5efe889e1e690bcb83f2cc4abe7ebdda5af569d1
RichieCesae <ricardo.ans29@gmail.com>
2023-10-27 16:17:16 +0100

    Fimaula

--- FichaJava/src/FichaExtra02/Ex_04_2.java
+++ FichaJava/src/FichaExtra02/Ex_04_2.java
@@ -4,9 +4,15 @@ public class Ex_04_2 {
 
     public static void main(String[] args) {
 
-        for (){
+        int starLimit=0;
 
-        }
+        for (int lines = 0; lines <= 6; lines++) {
+                for (int star = 1; star < starLimit; star++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+                starLimit++;
+            }
 
     }
 }
